dfc38059920fc18d2ef4b446c2362663f3f141f3
petrotechmeco-web <petrotechme.co@gmail.com>
2026-07-10 20:14:01 +0330

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from './components/Hero.jsx'
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      {/* بخش‌های بعدی به مرور اینجا اضافه می‌شوند */}
+    </>
+  )
+}
