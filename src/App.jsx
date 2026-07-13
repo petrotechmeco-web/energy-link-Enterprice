77c38ce34dd730bb2a2c34d2c495eaec8cb14ac9
petrotechmeco-web <petrotechme.co@gmail.com>
2026-07-13 05:42:28 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero';
 import AboutSolutions from './components/AboutSolutions';
+import IndustriesFooter from './components/IndustriesFooter';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-brand-dark selection:bg-brand-green selection:text-brand-dark">
       <Hero />
       <AboutSolutions />
+      <IndustriesFooter />
     </div>
   );
 }
